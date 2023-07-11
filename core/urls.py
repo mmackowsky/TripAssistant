e7504e8038ed896569f85913a82f2440ecaca0a8
mmackowsky <maciostah@gmail.com>
2023-07-11 10:06:57 +0200

    rename file

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for trip-assistant project.
+URL configuration for src project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from users import views as user_views
+from django.urls import path, include
+from assistant import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.UserRegistrationView.as_view(), name='register')
+    path('', include('assistant.urls'))
 ]
