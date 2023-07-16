315478c7306f86956b3abd66c65c43cec0d39a23
mmackowsky <maciostah@gmail.com>
2023-07-16 12:33:32 +0200

    feat(urls): update urls file

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,9 @@ from .views import signup, activate, login_request, logout_request
 
 
 urlpatterns = [
-    path("signup", signup, name="signup"),
+    path("signup/", signup, name="signup"),
     path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
          activate, name='activate'),
-    path("login", login_request, name="login")
+    path("login/", login_request, name="login"),
+    path("logout/", logout_request, name="logout")
 ]
