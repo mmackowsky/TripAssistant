481d68a550b551697538d397e89667bc3ae6cc75
mmackowsky <maciostah@gmail.com>
2023-07-18 20:22:22 +0200

    update

--- users/views.py
+++ users/views.py
@@ -23,23 +23,10 @@ def signup(request):
             user.save()
             to_email = form.cleaned_data.get('email')
             send_email(request, user, to_email)
-            # current_site = get_current_site(request)
-            # mail_subject = 'Activation link has been sent to your email id'
-            # message = render_to_string('acc_active_email.html', {
-            #     'user': user,
-            #     'domain': current_site.domain,
-            #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-            #     'token': account_activation_token.make_token(user),
-            # })
-            # to_email = form.cleaned_data.get('email')
-            # email = EmailMessage(
-            #     mail_subject, message, to=[to_email]
-            # )
-            # email.send()
             return HttpResponse('Please confirm your email address to complete registration')
-        else:
-            form = SignupForm()
-        return render(request, 'signup.html', {'form': form})
+    else:
+        form = SignupForm()
+    return render(request, 'users/signup.html', {'form': form})
 
 
 def send_email(request: HttpRequest, user: User, to_email: str) -> None:
@@ -57,16 +44,26 @@ def send_email(request: HttpRequest, user: User, to_email: str) -> None:
     email.send()
 
 
-def activate(request, uid64, token):
+def get_user_by_uid(uid64):
     User = get_user_model()
     try:
         uid = force_str(urlsafe_base64_decode(uid64))
-        user = User.objects.get(pk=uid)
+        return User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
+        return None
+
+
+def activate_user(user, token) -> bool:
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
+        return True
+    return False
+
+
+def activate(request, uid64, token):
+    user = get_user_by_uid(uid64)
+    if activate_user(user, token):
         return HttpResponse('Thank you for email confirmation. Now you can login your account')
     else:
         return HttpResponse('Activation link is invalid')
@@ -82,19 +79,19 @@ def login_request(request):
             if user is not None:
                 login(request, user)
                 messages.info(request, f'Hello {username}!')
-                return redirect('assistant/index.html')
+                return redirect('users/login.html')
             else:
                 messages.error(request, 'Invalid username or password.')
         else:
             messages.error(request, 'Invalid username or password.')
     form = AuthenticationForm()
-    return render(request=request, template_name='users/sign-in.html', context={'login_form': form})
+    return render(request=request, template_name='users/login.html', context={'login_form': form})
 
 
 def logout_request(request):
     logout(request)
     messages.info(request, 'Successfully logout.')
-    return redirect('assistant/index.html')
+    return redirect('users/login.html')
 
 
 # def profile(request):
