5665c7bb31ee9f9313b5394cb4fe44e84eb2e2cf
mmackowsky <maciostah@gmail.com>
2023-08-01 16:54:46 +0200

    feat(urls): update

--- core/urls.py
+++ core/urls.py
@@ -18,16 +18,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from assistant import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from users import views as user_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('assistant.urls')),
-    path('signup/', user_views.signup, name="signup"),
-    path('login/', user_views.login_request, name="login")
+    path('', include('users.urls')),
+    # path('signup/', user_views.signup, name="signup"),
+    # path('login/', user_views.login_request, name="login"),
+    path('social-auth/', include('social_django.urls', namespace="social")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
