eb34aed71d6dd8806ac10556ffe5ee2f13434758
mmackowsky <maciostah@gmail.com>
2023-08-03 18:15:01 +0200

    feat(urls): update

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import signup, activate, login_request, logout_request
 
 
 urlpatterns = [
     path("signup/", signup, name="signup"),
-    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+    path('activate/<uid64>/<token>',
          activate, name='activate'),
     path("login/", login_request, name="login"),
-    path("logout/", logout_request, name="logout")
+    path("logout/", logout_request, name="logout"),
+    # path('social-auth/', include('social_django.urls', namespace="social")),
 ]
