77c592512b39589ef6b137ad73fe3b216de09466
mmackowsky <maciostah@gmail.com>
2023-08-11 11:19:50 +0200

    feat(urls): add media_url, media_root

--- core/urls.py
+++ core/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +27,7 @@ urlpatterns = [
     # path('signup/', user_views.signup, name="signup"),
     # path('login/', user_views.login_request, name="login"),
     path('social-auth/', include('social_django.urls', namespace="social")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 urlpatterns += staticfiles_urlpatterns()
