c8cbf1d58407909bd50d0ab3f6053a87e0d1bd2e
mmackowsky <maciostah@gmail.com>
2023-08-11 11:20:47 +0200

    feat(views): add new view

--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+import requests
+from django.conf import settings
+from django.http import JsonResponse
 
-# Create your views here.
+
+# class GoogleAPI:
+#     def __init__(self, api_key):
+#         self.api_key = api_key
+#
+#
+#     def get_data(self, url, query):
+#         response = requests.get(url)
+#         data = response.json()
+
+def get_places(request):
+    api_key = settings.GOOGLE_API_KEY
+    query = ""
+    url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&key={api_key}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    return JsonResponse(data)
