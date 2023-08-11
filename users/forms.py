f8fdfa82ad1224443246c83b14af942acadc50d6
mmackowsky <maciostah@gmail.com>
2023-08-11 11:23:54 +0200

    feat(forms): add forms to update profile

--- users/forms.py
+++ users/forms.py
@@ -1,51 +1,33 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
+from .models import Profile
 
 
 class SignupForm(UserCreationForm):
-    username = forms.CharField(label='username', min_length=5, max_length=150)
-    email = forms.EmailField(label='email')
-    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+    username = forms.CharField(label='Username', min_length=5, max_length=150)
+    email = forms.EmailField(label='Email')
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-    #
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     for field_name in self.fields:
-    #         self.fields[field_name].widget.attrs.update({'class': 'form-control'})
 
-    # def username_clean(self):
-    #     username = self.cleaned_data['username'].lower()
-    #     new = User.objects.filter(username=username)
-    #     if new.count():
-    #         raise ValidationError("User Already Exist")
-    #     return username
-    #
-    # def email_clean(self):
-    #     email = self.cleaned_data['email'].lower()
-    #     new = User.objects.filter(email=email)
-    #     if new.count():
-    #         raise ValidationError(" Email Already Exist")
-    #     return email
-    #
-    # def clean_password2(self):
-    #     password1 = self.cleaned_data['password1']
-    #     password2 = self.cleaned_data['password2']
-    #
-    #     if password1 and password2 and password1 != password2:
-    #         raise ValidationError("Password don't match")
-    #     return password2
-    #
-    # def save(self, commit=True):
-    #     user = User.objects.create_user(
-    #         self.cleaned_data['username'],
-    #         self.cleaned_data['email'],
-    #         self.cleaned_data['password1']
-    #     )
-    #     return user
+class UserUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(label='first_name', min_length=3, max_length=25)
+    last_name = forms.CharField(label='last_name', min_length=2, max_length=50)
+    username = forms.CharField(label='username', min_length=5, max_length=150)
+    email = forms.EmailField(label='email')
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['image']
