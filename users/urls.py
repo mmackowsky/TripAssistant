bab865953acca025cf1b0dab34efccb99fd2f179
mmackowsky <maciostah@gmail.com>
2023-08-11 11:25:40 +0200

    feat(urls): password reset routes

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from .views import signup, activate, login_request, logout_request
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
 
+from .views import (ResetPasswordView, activate, login_request, logout_request,
+                    signup, profile)
 
 urlpatterns = [
     path("signup/", signup, name="signup"),
@@ -9,5 +11,13 @@ urlpatterns = [
          activate, name='activate'),
     path("login/", login_request, name="login"),
     path("logout/", logout_request, name="logout"),
+    path('profile/', profile, name="profile"),
+    path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
     # path('social-auth/', include('social_django.urls', namespace="social")),
 ]
