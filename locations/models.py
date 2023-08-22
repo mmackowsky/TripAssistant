bc5ee82d1a450f6e5c8d3fbc7b3a5cc4dc99e0e1
mmackowsky <maciostah@gmail.com>
2023-08-22 11:42:34 +0200

    feat(models): rename fields

--- locations/models.py
+++ locations/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Location(models.Model):
-    place = models.CharField(max_length=50)
-    city = models.CharField(max_length=85)
+    place_type = models.CharField(max_length=50)
+    location_name = models.CharField(max_length=85)
     street = models.CharField(max_length=50)
     zipcode = models.CharField(max_length=10)
     country = models.CharField(max_length=50)
