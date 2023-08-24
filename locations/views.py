47bd168edb6f8d9f8b1a7daecdc8fa9a91b8f159
mmackowsky <maciostah@gmail.com>
2023-08-24 10:17:25 +0200

    feat(views): update views

--- locations/views.py
+++ locations/views.py
@@ -1,60 +1,95 @@
 import json
-
+from typing import Any, Dict
 import requests
 from django.contrib import messages
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.core.paginator import Paginator
 
 
 from .forms import SearchForm
+from .models import Location
 from core import settings
 
 
-def search(request):
-    if request.method == "POST":
+class LocationListView(ListView):
+    model = Location
+    context_object_name = 'locations'
+    template_name = 'home.html'
+    paginate_by = 6
+
+    def __parse_response_data(self, data):
+        parsed_data = []
+
+        for item in data:
+            parsed_item = {
+                'location_name': item['name'],
+                'place_type': item['type'],
+                'city': item['address'].get('city') or item['address'].get('town'),
+                'street': item['address']['road'],
+                'postcode': item['address']['postcode']
+            }
+            parsed_data.append(parsed_item)
+
+        self.__save_to_db(parsed_data)
+
+    def _get_from_api(self, place_type, location_name):
+        query = f"{place_type} near {location_name}"
+        api_url = f'https://nominatim.openstreetmap.org/search?q={query}&format=json&addressdetails=1&limit=9&key={settings.NOMINATIM_API_KEY}'
+        data = requests.get(api_url).json()
+        self.__parse_response_data(data)
+
+    def __save_to_db(self, data):
+        Location.objects.bulk_create([
+            Location(**item) for item in data
+        ])
+
+    def get_queryset(self):
+        queryset = Location.objects.all()
+        form = SearchForm(self.request.POST or None)
+        if form.is_valid():
+            location_name = form.cleaned_data.get('location_name')
+            place_type = form.cleaned_data.get('place_type')
+
+            if location_name and place_type:
+                queryset = queryset.filter(city__icontains=location_name, place_type=place_type)
+        return queryset
+
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['form'] = SearchForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
         form = SearchForm(request.POST)
         if form.is_valid():
             location_name = form.cleaned_data.get('location_name')
             place_type = form.cleaned_data.get('place_type')
-            query = f"{place_type} near {location_name}"
-            api_url = f'https://nominatim.openstreetmap.org/search?q={query}&format=json&limit=12&key={settings.NOMINATIM_API_KEY}'
-            data = requests.get(api_url).json()
-            return data
-    else:
-        form = SearchForm()
-    return render(request, 'home.html', {'form': form})
+            self._get_from_api(place_type, location_name)
+
+            queryset = self.get_queryset()
+
+            # paginator = Paginator(queryset, self.paginate_by)
 
 
+            return render(request, 'home.html', {'form': form, 'locations': queryset})
+        return render(request, 'home.html', {'form': form})
 
-    # def get_query(self, city_name, place_type):
-    #     location = self.GEOLOCATOR.geocode(city_name)
-    #
-    #     if location:
-    #         lat = location.latitude
-    #         lon = location.longitude
-    #         query = f"{place_type} near {lat}, {lon}"
-    #         return query, lat, lon
-    #     else:
-    #         return None
-    #
-    # def get_places(self, request, radius=5):
-    #     if request.method == "GET":
-    #         form = PlaceForm(request.GET)
+
+    # @staticmethod
+    # def search(request):
+    #     if request.method == "POST":
+    #         form = SearchForm(request.POST)
     #         if form.is_valid():
-    #             city_name = form.cleaned_data.get('city_name')
+    #             location_name = form.cleaned_data.get('location_name')
     #             place_type = form.cleaned_data.get('place_type')
-    #             query, lat, lon = self.get_query(city_name=city_name, place_type=place_type)
-    #             try:
-    #                 places = self.GEOLOCATOR.geocode(query, exactly_one=False, limit=None)
-    #                 if places:
-    #                     for place in places:
-    #                         place_coords = (place.latitude, place.longitude)
-    #                         place_distance = geodesic((lat, lon), place_coords).kilometers
-    #                         if place_distance <= radius:
-    #                             print(f'{place.address} ({place_distance:.2f} km)')
-    #                             return place.address
-    #                 else:
-    #                     return messages.warning(request, "No nearby places found for the given type.")
-    #             except:
-    #                 return messages.error(request, "Error: Unable to fetch nearby places.")
-
+    #             query = f"{place_type} near {location_name}"
+    #             api_url = f'https://nominatim.openstreetmap.org/search?q={query}&format=json&addressdetails=1&limit=9&key={settings.NOMINATIM_API_KEY}'
+    #             data = requests.get(api_url).json()
+    #             parsed_data = parse_response_data(data)
+    #             return render(request, 'home.html', {'form': form, 'data': parsed_data})
+    #     else:
+    #         form = SearchForm()
+    #     return render(request, 'home.html', {'form': form})
