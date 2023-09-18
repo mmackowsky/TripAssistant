6b91bf0f19f5f390948261d2414cb8d859ebb808
mmackowsky <maciostah@gmail.com>
2023-09-18 11:19:28 +0200

    feat(forms): add multiupload form

--- src/locations_images/forms.py
+++ src/locations_images/forms.py
@@ -1,9 +1,22 @@
 from django import forms
+from multiupload.fields import MultiImageField
+
 from .models import Images
 
 
 class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Images
+        fields = ["image"]
+
+
+class MultiUploadForm(forms.Form):
+    attachments = MultiImageField(min_num=1, max_num=3, max_file_size=1024 * 1024 * 5)
 
     class Meta:
         model = Images
-        fields = ['image']
+        fields = ["image"]
+
+    def __init__(self, *args, **kwargs):
+        super(MultiUploadForm, self).__init__(*args, **kwargs)
+        self.fields["attachments"].validators = []
