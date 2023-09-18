1ba39dc4f17bc80f6cb8ed48168aaafb950f26a1
mmackowsky <maciostah@gmail.com>
2023-09-18 11:21:13 +0200

    feat(models): update

--- src/locations_images/models.py
+++ src/locations_images/models.py
@@ -7,10 +7,14 @@ from users.models import Profile
 
 
 class Images(models.Model):
-    image = models.ImageField(default='places/default_place_image.jpg', upload_to='places')
+    image = models.FileField(
+        default="places/default_place_image.jpg", upload_to="places"
+    )
     uploaded_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(default=timezone.now)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, default=None)
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE, default=None, related_name="images"
+    )
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
