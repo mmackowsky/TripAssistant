ca9452148e8fb89961ba4ae947b804e54e6d0f72
mmackowsky <maciostah@gmail.com>
2023-09-19 11:58:54 +0200

    feat(models): update

--- src/users/models.py
+++ src/users/models.py
@@ -1,14 +1,15 @@
-from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
+from django.db import models
 from PIL import Image
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
-    image = models.ImageField(default='user_profile/default.jpg', upload_to='user_profile')
+    image = models.ImageField(
+        default="user_profile/default.jpg", upload_to="user_profile"
+    )
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs) -> None:
         super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
@@ -18,20 +19,5 @@ class Profile(models.Model):
             img.thumbnail(output_size)
             img.save(self.image.path)
 
-    def __str__(self):
-        return f'{self.user.username}'
-
-
-# class Images(models.Model):
-#     image = models.ImageField()
-#     uploaded_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
-#     date_posted = models.DateTimeField(default=timezone.now)
-#     location = models.ForeignKey(Location, on_delete=models.CASCADE, default=None)
-#
-#
-# class Reviews(models.Model):
-#     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-#     content = models.CharField(max_length=500)
-#     date_posted = models.DateTimeField(default=timezone.now)
-#     stars = models.PositiveIntegerField()
-#     location = models.ForeignKey(Location, on_delete=models.CASCADE, default=None)
+    def __str__(self) -> str:
+        return f"{self.user.username}"
