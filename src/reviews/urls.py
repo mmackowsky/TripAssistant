66d869e7c6149e79707810804305f75362e45976
mmackowsky <maciostah@gmail.com>
2023-09-19 10:32:27 +0200

    feat(urls): update urls

--- src/reviews/urls.py
+++ src/reviews/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ReviewListView, add_review, get_reviews
+from .views import ReviewDeleteView, ReviewUpdateView, add_review, get_reviews
 
 urlpatterns = [
-    path('reviews/<int:location_id>', get_reviews, name="reviews"),
-    path('add_review/<int:location_id>', add_review, name='add-review')
+    path("reviews/<int:location_id>", get_reviews, name="reviews"),
+    path("reviews/<int:location_id>/create", add_review, name="add-review"),
+    path("reviews/<int:pk>/delete", ReviewDeleteView.as_view(), name="delete-review"),
+    path("reviews/<int:pk>/edit", ReviewUpdateView.as_view(), name="edit-review"),
 ]
