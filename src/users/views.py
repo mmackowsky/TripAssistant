03e3ae7c154df0259b1e879e1ed06d2156d2c8ed
mmackowsky <maciostah@gmail.com>
2023-09-19 11:56:10 +0200

    feat(views): update

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from django.contrib import messages
 from django.contrib.auth import authenticate, get_user_model, login, logout
 from django.contrib.auth.decorators import login_required
@@ -14,51 +16,54 @@ from django.urls import reverse_lazy
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
-from .forms import SignupForm, ProfileUpdateForm, UserUpdateForm
+from .forms import ProfileUpdateForm, SignupForm, UserUpdateForm
 from .tokens import account_activation_token
 
 
-def signup(request):
-    if request.method == 'POST':
+def signup(request: HttpRequest) -> Union[render, redirect]:
+    if request.method == "POST":
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.is_active = False
             user.save()
-            to_email = form.cleaned_data.get('email')
+            to_email = form.cleaned_data.get("email")
             send_email(request, user, to_email)
-            messages.info(request, 'Please confirm your email address to complete registration')
-            return redirect('login')
+            messages.info(
+                request, "Please confirm your email address to complete registration"
+            )
+            return redirect("login")
     else:
         form = SignupForm()
-    return render(request, 'users/signup.html', {'form': form})
+    return render(request, "users/signup.html", {"form": form})
 
 
 def send_email(request: HttpRequest, user: User, to_email: str) -> None:
     current_site = get_current_site(request)
-    mail_subject = 'Activation link has been sent to your email id'
-    message = render_to_string('users/acc_active_email.html', {
-        'user': user,
-        'domain': current_site.domain,
-        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-        'token': account_activation_token.make_token(user),
-    })
-    email = EmailMessage(
-        mail_subject, message, to=[to_email]
+    mail_subject = "Activation link has been sent to your email id"
+    message = render_to_string(
+        "users/acc_active_email.html",
+        {
+            "user": user,
+            "domain": current_site.domain,
+            "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+            "token": account_activation_token.make_token(user),
+        },
     )
+    email = EmailMessage(mail_subject, message, to=[to_email])
     email.send()
 
 
-def get_user_by_uid(uid64):
+def get_user_by_uid(uid64: str) -> Union[User, None]:
     User = get_user_model()
     try:
         uid = force_str(urlsafe_base64_decode(uid64))
         return User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
         return None
 
 
-def activate_user(user, token) -> bool:
+def activate_user(user: User, token: str) -> bool:
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
@@ -66,65 +71,77 @@ def activate_user(user, token) -> bool:
     return False
 
 
-def activate(request, uid64, token):
+def activate(request: HttpRequest, uid64: str, token: str) -> Union[redirect, render]:
     user = get_user_by_uid(uid64)
     if activate_user(user, token):
-        messages.success(request, 'Thank you for email confirmation. Now you can login your account')
-        return redirect('login')
-    messages.warning(request, 'Activation link is invalid')
-    return redirect('signup')
+        messages.success(
+            request, "Thank you for email confirmation. Now you can login your account"
+        )
+        return redirect("login")
+    messages.warning(request, "Activation link is invalid")
+    return redirect("signup")
 
 
-def login_request(request):
+def login_request(request: HttpRequest) -> Union[render, redirect]:
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.info(request, f'Hello {username}!')
-                return redirect('home')
+                messages.info(request, f"Hello {username}!")
+                return redirect("home")
             else:
-                messages.error(request, 'Invalid username or password.')
+                messages.error(request, "Invalid username or password.")
         else:
-            messages.error(request, 'Invalid username or password.')
+            messages.error(request, "Invalid username or password.")
     else:
         form = AuthenticationForm()
-    return render(request=request, template_name='users/login.html', context={'login_form': form})
+    return render(
+        request=request, template_name="users/login.html", context={"login_form": form}
+    )
 
 
-def logout_request(request):
+def logout_request(request: HttpRequest) -> redirect:
     logout(request)
-    messages.info(request, 'Successfully logout.')
-    return redirect('login')
+    messages.info(request, "Successfully logout.")
+    return redirect("login")
 
 
 @login_required
-def profile(request):
-    if request.method == 'POST':
+def profile(request: HttpRequest) -> Union[render, redirect]:
+    if request.method == "POST":
         user_form = UserUpdateForm(request.POST, instance=request.user)
-        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
 
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
             messages.success(request, "Your profile has been updated!")
-            return redirect('profile')
+            return redirect("profile")
     else:
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.profile)
 
-    return render(request, 'users/profile.html', {'user_form': user_form, 'profile_form': profile_form})
+    return render(
+        request,
+        "users/profile.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
 
 
 class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
-    template_name = 'users/password_reset.html'
-    email_template_name = 'users/password_reset_email.html'
-    subject_template_name = 'users/password_reset_subject.txt'
-    success_message = "We've emailed you instructions for setting your password, " \
-                      "if an account exists with the email you entered. You should receive them shortly." \
-                      " If you don't receive an email, " \
-                      "please make sure you've entered the address you registered with, and check your spam folder."
-    success_url = reverse_lazy('home')
+    template_name = "users/password_reset.html"
+    email_template_name = "users/password_reset_email.html"
+    subject_template_name = "users/password_reset_subject.txt"
+    success_message = (
+        "We've emailed you instructions for setting your password, "
+        "if an account exists with the email you entered. You should receive them shortly."
+        " If you don't receive an email, "
+        "please make sure you've entered the address you registered with, and check your spam folder."
+    )
+    success_url = reverse_lazy("home")
