7db07726f42de6666b9d2c4250577fdbacf40c7e
mmackowsky <maciostah@gmail.com>
2023-09-25 17:46:23 +0200

    tests: add new tests

--- src/users/tests.py
+++ src/users/tests.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase
 from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 from .models import Profile
-from .views import save_to_s3
+from .views import save_to_s3, get_user_by_uid
 
 
 class UsersViewsTest(TestCase):
@@ -67,3 +70,37 @@ class UsersViewsTest(TestCase):
             reverse("password-reset"), {"email": "test@example.com"}
         )
         self.assertEqual(response.status_code, 302)
+
+    def test_activate_view(self):
+        # Create a user to activate
+        user = User.objects.create_user(
+            username="newuser", email="newuser@example.com", password="newpassword123"
+        )
+
+        # Generate an activation token
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+
+        response = self.client.get(reverse("activate", args=[uid, token]))
+        self.assertEqual(response.status_code, 302)
+        user.refresh_from_db()
+        self.assertTrue(user.is_active)
+
+    def test_get_user_by_uid(self):
+        user = User.objects.create_user(
+            username="newuser", email="newuser@example.com", password="newpassword123"
+        )
+
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+
+        # Test with valid UID
+        result = get_user_by_uid(uid)
+        self.assertEqual(result, user)
+
+        # Test with invalid UID
+        result = get_user_by_uid("invalid_uid")
+        self.assertIsNone(result)
+
+    def test_reset_password_view_template_used(self):
+        response = self.client.get(reverse("password-reset"))
+        self.assertTemplateUsed(response, "users/password_reset.html")
