469d0ee9d1f4df0e48cf6213acbbd12509f4ad88
mmackowsky <maciostah@gmail.com>
2023-09-25 15:53:18 +0200

    feat(models): update

--- src/users/models.py
+++ src/users/models.py
@@ -5,19 +5,9 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
-    image = models.ImageField(
+    image = models.FileField(
         default="user_profile/default.jpg", upload_to="user_profile"
     )
-
-    def save(self, *args, **kwargs) -> None:
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
 
     def __str__(self) -> str:
         return f"{self.user.username}"
