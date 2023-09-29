36a1ad7417483856f2febd8afe0d26f3958372a7
mmackowsky <maciostah@gmail.com>
2023-09-29 17:37:16 +0200

    settings: update

--- src/core/settings.py
+++ src/core/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["trip-assistant.eu-north-1.elasticbeanstalk.com"]
 
 
 # Application definition
