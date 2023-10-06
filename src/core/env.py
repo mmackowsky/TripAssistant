86d2fc9c1df28722bc3cfd8250a7fcaca8ac9174
mmackowsky <maciostah@gmail.com>
2023-10-06 12:54:36 +0200

    update

--- src/core/env.py
+++ src/core/env.py
@@ -4,4 +4,5 @@ from django.core.management.utils import get_random_secret_key
 env = environ.Env(
     DEBUG=(bool, False),
     ENVIRONMENT=(str, "DEV"),
+    SECRET_KEY=(str, get_random_secret_key()),
 )
