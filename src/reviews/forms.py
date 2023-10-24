b088f45701e597b503a097f3db6553edd1874812
mmackowsky <maciostah@gmail.com>
2023-10-24 17:07:36 +0200

    feat(forms): update

--- src/reviews/forms.py
+++ src/reviews/forms.py
@@ -4,8 +4,8 @@ from .models import Reviews
 
 
 class ReviewForm(forms.ModelForm):
-    content = forms.CharField(max_length=500)
-    stars = forms.IntegerField()
+    content = forms.CharField(max_length=500, required=True, error_messages={'required': 'Content is required.'})
+    stars = forms.IntegerField(required=True, error_messages={'required': 'Stars are required.'})
 
     class Meta:
         model = Reviews
