fdff18d67d3201e7d93c971b5beca0f4f97c0fd4
mmackowsky <maciostah@gmail.com>
2023-10-25 19:25:36 +0200

    update

--- src/core/settings.py
+++ src/core/settings.py
@@ -184,6 +184,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
+MAX_ATTACHMENT_SIZE = 1024 * 1024 * 5
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
