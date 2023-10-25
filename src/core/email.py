37f3e08f6f8ad766d0c6d2814964793627d78203
mmackowsky <maciostah@gmail.com>
2023-10-25 19:17:17 +0200

    delete

--- src/core/email.py
+++
@@ -1,5 +0,0 @@
-def send_templated_email(to_email: str, template: str, context) -> None:
-    mail_subject = "Activation link has been sent to your email id"
-    message = render_to_string(template, context)
-    email = EmailMessage(mail_subject, message, to=[to_email])
-    email.send()
